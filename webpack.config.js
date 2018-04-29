8df19c76fa65020401659dae67e487cc69506376
drdlee <danielrudi.lee@gmail.com>
2018-04-30 05:50:35 +0800

    pakai HTMLWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const HTMLwp = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'hasil.bundle.js'
-  }
+  },
+  plugins: [
+    new HTMLwp({
+      title: 'Lalalalnd',
+      template: './src/my-index.html',
+      minify: { collapseWhitespace: true }
+    })
+  ]
 };
